d60355cd8a9074e7109a0d8e113e619dd0393dfa
deprekate <deprekate@gmail.com>
2020-10-01 16:29:56 -0700

    changed it so start codons are not included

--- main.py
+++ main.py
@@ -86,8 +86,14 @@ def xx(arr):
     return np.median(np.abs(arr - med))
 
 def xxx(data, axis=None):
+	#print( np.median(data - np.mean(data, axis), axis) )
 	return np.absolute(np.median(data - np.mean(data, axis), axis))
 setattr(np, 'xxx', xxx)
+
+def sums(data, axis=None):
+	return np.sum(np.absolute(data - np.mean(data, axis)), axis)
+setattr(np, 'sums', sums)
+
 
 def aad(data, axis=None):
 	return np.mean(np.absolute(data - np.mean(data, axis)), axis)
@@ -141,6 +147,9 @@ parser.add_argument('-d','--data_type', action="store", dest="data_type", requir
 parser.add_argument('-t','--clust_type', action="store", dest="clust_type", required=True)
 parser.add_argument('-n','--clust_num', action="store", dest="clust_num", required=True, type=int)
 parser.add_argument('-a','--annotate', action="store_true", dest="annotate", required=False)
+parser.add_argument('-o','--offset', action="store_true", dest="offset", required=False)
+parser.add_argument('-m','--maxd', type=int, default=80, dest="maxd", required=False)
+
 args = parser.parse_args()
 
 #-----------------------READ IN THE DATA
@@ -218,6 +227,11 @@ for index,row in dat[['START','STOP']].iterrows():
 dat['OFFSET'] = offset
 dat.loc[dat.TYPE==True, 'OFFSET'] = True
 
+
+dat.loc[dat.CODON=='ATG', 'M'] = dat.loc[dat.CODON=='ATG', 'M'] - 1
+dat.loc[dat.CODON=='GTG', 'V'] = dat.loc[dat.CODON=='GTG', 'V'] - 1
+dat.loc[dat.CODON=='TTG', 'L'] = dat.loc[dat.CODON=='TTG', 'L'] - 1
+
 X = dat[list("ARNDCEQGHILKMFPSTWYV")]
 X = X.div(X.sum(axis=1), axis=0)
 #X.loc[:, list("ARNDCEQGHILKMFPSTWYV")]  = StandardScaler().fit_transform(X)
@@ -261,11 +275,16 @@ elif(args.clust_type == 'gm'):
 	dat['CLUSTER'] = model.predict(X)
 elif(args.clust_type == 'bgm'):
 	dat['CLUSTER'] = BayesianGaussianMixture(n_components=args.clust_num,covariance_type='spherical').fit_predict(X)
+elif(args.clust_type == 'h'):
+	from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+	Z = linkage(X, 'ward')
+	print(args.maxd)
+	dat['CLUSTER'] = fcluster(Z, args.maxd, criterion='distance') - 1
 elif(args.clust_type == 'ag'):
 	#dat['CLUSTER'] = AgglomerativeClustering(n_clusters=args.clust_num, affinity='cosine', linkage='complete').fit_predict(X)
 	dat['CLUSTER'] = AgglomerativeClustering(n_clusters=None, distance_threshold=1.8, affinity='cosine', linkage='complete').fit_predict(X)
 elif(args.clust_type == 'ward'):
-	dat['CLUSTER'] = AgglomerativeClustering(n_clusters=args.clust_num, linkage='ward').fit_predict(X)
+	dat['CLUSTER'] = AgglomerativeClustering(n_clusters=args.clust_num, linkage='ward', compute_full_tree=True).fit_predict(X)
 elif(args.clust_type == 'pam'):
 	dat['CLUSTER'] = KMedoids(n_clusters=args.clust_num).fit_predict(X)
 elif(args.clust_type == 'op'):
@@ -275,6 +294,7 @@ elif(args.clust_type == 'sp'):
 elif(args.clust_type == 'b'):
 	dat['CLUSTER'] = Birch(n_clusters=args.clust_num).fit_predict(X)
 
+print(dat)
 
 pca = decomposition.PCA(n_components=2).fit(X)
 dat['x'],dat['y'] = pca.fit_transform(X).T
@@ -287,13 +307,14 @@ v = [np.sum(np.mmd(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ;
 v = [np.sum(np.aad(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('aad', v)
 v = [np.median(np.mad(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('mmm', v)
 v = [np.linalg.norm(np.mad(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('euc', v)
-v = [np.sum(np.mad(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('mad', v)
 v = [np.sum(np.xxx(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('xxx', v)
+v = [np.sum(np.sums(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('sum', v)
+v = [np.sum(np.mad(X[dat.CLUSTER==i], axis=0)) for i in range(args.clust_num)] ; print('mad', v)
 l = [len(X[dat.CLUSTER==i]) for i in range(args.clust_num)]
 print('   len', l)
 #index_min = np.argmin(variance)
-index_min = np.argmin(v)
-#index_min = get_best(v, l)
+#index_min = np.argmin(v)
+index_min = get_best(v, l)
 print(index_min)
 #index_mid = np.argmed(variance)
 #index_max = np.argmax(variance)
@@ -331,34 +352,47 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from matplotlib.patches import Ellipse
-fig, ax = plt.subplots(figsize=(4,3.5))
+fig, ax = plt.subplots(figsize=(3.97,3.49))
 #fig.set_size_inches(4, 2.5)
 
+'''
+dendrogram(
+    Z,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8.,  # font size for the x axis labels
+)
+fig.savefig(args.genome_id + '.png', dpi=300, bbox_inches='tight')
+exit()
+'''
+
+
 # PLOT
-colors = {True:'#3CC9CF', False:'#F2766E'}
-#colors = {True:'#3CC9CF', 'IG':'black', '-0':'b', '1':'g', '2':'r', '-1':'m', '-2':'y'}
 markers = {k:v for k,v in zip({0,1,2,3}.difference({index_min}), ['o','s','H'])}
 
 #fig.suptitle(args.genome_id + " (" + title + ")")
 fig.suptitle(args.genome_id)
-ax.scatter(dat['x'], dat['y'], c=dat['TYPE'].apply(lambda x: colors[x]), marker='.', linewidths=0.0, alpha=0.4, zorder=5)
-#ax.scatter(dat['x'], dat['y'], c=dat['OFFSET'].apply(lambda x: colors[x]), marker='.', linewidths=0.0, alpha=0.4, zorder=5)
 ax.set_xlabel('PC1', fontsize=12)
 plt.ylabel('PC2')
 
-#for label in ['1', '2', '-0', '-1', '-2']:
-#	confidence_ellipse(dat.loc[dat.OFFSET == label, 'x'], dat.loc[dat.OFFSET == label, 'y'], ax, edgecolor=colors[label], linewidth=0.5, zorder=0)
-
 if(args.annotate):
+	colors = {True:'#3CC9CF', False:'#F2766E'}
+	ax.scatter(dat['x'], dat['y'], c=dat['TYPE'].apply(lambda x: colors[x]), marker='.', linewidths=0.0, alpha=0.4, zorder=5)
 	p = dat[(dat.NUM==1) & (dat.CLUSTER!=6)] #index_min]
 	for i, row in p.iterrows():
 		ax.annotate(str(row.STOP), (row.x, row.y), size=2, zorder=10)
 		print(row.STOP)
+elif(args.offset):
+	colors = {True:'#3CC9CF', 'IG':'black', '-0':'b', '1':'g', '2':'r', '-1':'m', '-2':'y'}
+	ax.scatter(dat['x'], dat['y'], c=dat['OFFSET'].apply(lambda x: colors[x]), marker='.', linewidths=0.0, alpha=0.4, zorder=5)
+	for label in ['1', '2', '-0', '-1', '-2']:
+		confidence_ellipse(dat.loc[dat.OFFSET == label, 'x'], dat.loc[dat.OFFSET == label, 'y'], ax, edgecolor=colors[label], linewidth=0.5, zorder=0)
 else:
+	colors = {True:'#3CC9CF', False:'#F2766E'}
+	ax.scatter(dat['x'], dat['y'], c=dat['TYPE'].apply(lambda x: colors[x]), marker='.', linewidths=0.0, alpha=0.4, zorder=5)
 	ax.scatter(dat[dat.CLUSTER==index_min].x, dat[dat.CLUSTER==index_min].y, facecolor='none', cmap='Spectral', s=80, linewidths=0.3, alpha=0.3, marker='d', edgecolor='black', label='Predicted', zorder=10)
 	for i in range(args.clust_num):
 		if i != index_min:
-			ax.scatter(dat[dat.CLUSTER==i].x, dat[dat.CLUSTER==i].y, facecolor='none', cmap='Spectral', s=80, linewidths=0.3, alpha=0.5, marker=markers[i], edgecolor='green', label='Predicted', zorder=10)
+			ax.scatter(dat[dat.CLUSTER==i].x, dat[dat.CLUSTER==i].y, facecolor='none', cmap='Spectral', s=80, linewidths=0.3, alpha=0.5, marker=markers.get(i, '1'), edgecolor='green', label='Predicted', zorder=10)
 			pass
 	pass
 
